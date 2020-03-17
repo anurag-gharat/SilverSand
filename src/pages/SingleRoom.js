de9c3e56cf688c51a3f11dcec0adf6c01627d9c5
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-03-18 00:59:04 +0530

    "Done for today"

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react'
+import Hero from '../components/Hero'
+import defaultBCG from '../images/room-1.jpeg'
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
+import {RoomContext} from '../context'
+
 
 export default class SingleRoom extends Component {
-    render() {
+
+    constructor(props){
+        super(props)
+        this.state={
+            slug:this.props.match.params.slug,
+            defaultBCG
+        };
+    
+    }
+    static contextType=RoomContext;
+
+    render() {    
+
+        const {getRoom}=this.context
+        const room=getRoom(this.state.slug)
+        if(!room){
+            return<div className="error">
+                <h3>No such page can be found</h3>
+                <Link to="/rooms" className="btn-primary">Back To Rooms</Link>
+            </div>
+        }
+        const {name,description,capacity,size,price,extras,breakfast,pets,images}=room;
         return (
             <div>
-                single room page
+                    <Hero hero="roomsHero">
+                        <Banner title={`${name} room`}>
+                            <Link to="/rooms" className="btn-primary">Back to Rooms</Link>
+                        </Banner>
+                    </Hero>
             </div>
         )
     }
