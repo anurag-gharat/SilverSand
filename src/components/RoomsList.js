aceafc96bc3aa59b504ba3ccf5c481bcafab1a0f
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-12-01 22:36:48 +0530

    made changes in error message

--- src/components/RoomsList.js
+++ src/components/RoomsList.js
@@ -1,25 +1,21 @@
-import React from 'react'
-import Room from './Room'
-export default function RoomsList({rooms}) {
-    if(rooms.length===0){
-        return(
-            <div className="empty-search">
-               <h3> Layki nahi tuzi bhadyaaaaa! Nikal Lavde!</h3>
-            </div>
-        )
-    } 
-    
+import React from "react";
+import Room from "./Room";
+export default function RoomsList({ rooms }) {
+  if (rooms.length === 0) {
     return (
-        <section className="roomslist">
-            <div className="roomslist-center">
-                {
-                    rooms.map(item=>{
-                        return(
-                            <Room key={item.id} room={item}></Room>
-                        )
-                    })
-                }
-            </div>
-        </section>
-    )
+      <div className="empty-search">
+        <h3> Sorry no requested option available</h3>
+      </div>
+    );
+  }
+
+  return (
+    <section className="roomslist">
+      <div className="roomslist-center">
+        {rooms.map((item) => {
+          return <Room key={item.id} room={item}></Room>;
+        })}
+      </div>
+    </section>
+  );
 }
